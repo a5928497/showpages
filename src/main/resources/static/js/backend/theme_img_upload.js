3670eea4cf06ba785611800d1baa97f077170a32
a5928497 <61722615@qq.com>
2018-11-23 23:39:41 +0800

    developing

--- src/main/resources/static/js/backend/theme_img_upload.js
+++ src/main/resources/static/js/backend/theme_img_upload.js
@@ -12,22 +12,35 @@ $(function () {
     var pathName=window.document.location.pathname;
     var pos=curWwwPath.indexOf(pathName);
     var localhostPaht=curWwwPath.substring(0,pos);
+    var img_width;
+    var img_height;
     //根据路径获取图片
+    $sizeTips = $("#size_tips");
     $prevImg = $("#prevImg");
+    $themeImg = $("#theme_img");
+    var img_url  = localhostPaht + "/welcomeImg/";
     if ($prevImg.length != 0) {
         $("option[value='"+ $prevImg.val()+"']").attr("selected","selected");
-
     }
-    var img_url  = localhostPaht + "/welcomeImg/";
-    $themeImg = $("#theme_img");
-    $themeImg.attr("src",img_url+$(":selected").val()+"?"+Math.random()).css("width",resizeImg($(this).width()));
+    $themeImg.attr("src",img_url+$(":selected").val()+"?"+Math.random())
+        .load(function () {
+            img_width = $(this)[0].naturalWidth;
+            img_height = $(this)[0].naturalHeight;
+            $sizeTips.text("原始尺寸（像素）：宽" + img_width + "px，高" + img_height + "px");
+            $(this).css("width",resizeImg(img_width));
+
+        });
 
     //切换选项时切换图片
     $("select").change(function () {
+        $("option[selected='selected']").removeAttr("selected");
         $themeImg.removeAttr("style")
             .attr("src",img_url+$(":selected").val()+"?"+Math.random())
             .load(function () {
-                $themeImg.css("width",resizeImg($(this).width()));
+                img_width = $(this)[0].naturalWidth;
+                img_height = $(this)[0].naturalHeight;
+                $sizeTips.text("原始尺寸（像素）：宽" + img_width + "px，高" + img_height + "px");
+                $(this).css("width",resizeImg(img_width));
             });
     });
     //调整图片函数
